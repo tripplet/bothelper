75f132fc373f2e6b67c826433d971a2ad78951ff
Tobias Tangemann <tobiastangemann@gmail.com>
2016-07-26 01:01:16 +0200

    Refactoring + fix style warnings

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = 'ttobias'
-
 from datetime import datetime
 import logging
 import yaml
 
 import telegram  # pip install python-telegram-bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+__author__ = 'ttobias'
 
 # Enable telegram bot logging
 logging.basicConfig(level=logging.ERROR,
@@ -38,15 +38,15 @@ class TelegramBot(object):
             # Get the dispatcher to register handlers
             self.dispatcher = self.updater.dispatcher
 
-            self.dispatcher.add_error_handler(self.bot_error)
+            self.dispatcher.add_error_handler(TelegramBot.bot_error)
 
             # add basic commands
             self.dispatcher.add_handler(MessageHandler([Filters.text], self.rx_message))
 
-            self.dispatcher.add_handler(CommandHandler("info",   self.cmd_info))
-            self.dispatcher.add_handler(CommandHandler("start",  self.cmd_start))
+            self.dispatcher.add_handler(CommandHandler("info", self.cmd_info))
+            self.dispatcher.add_handler(CommandHandler("start", self.cmd_start))
             self.dispatcher.add_handler(CommandHandler("cancel", self.cmd_cancel))
-            self.dispatcher.add_handler(CommandHandler("help",   self.cmd_help))
+            self.dispatcher.add_handler(CommandHandler("help", self.cmd_help))
         except Exception as exp:
             print('Error creating telegram bot' + str(exp.strerr))
 
@@ -67,7 +67,7 @@ class TelegramBot(object):
         self.messages += 1
 
     def is_authorized(self, bot, update):
-        if not update.message.chat_id in self.cfg["users"]:
+        if update.message.chat_id not in self.cfg["users"]:
             self.send_message(update.message.chat_id, text='Unauthorized: %d' % update.message.chat_id)
             return False
 
@@ -86,7 +86,8 @@ class TelegramBot(object):
         return False
 
     def cmd_info(self, bot, update):
-        if not self.is_authorized(bot, update): return
+        if not self.is_authorized(bot, update):
+            return
         self.send_message(update.message.chat_id,
                           text='Version: {}\n'
                                'Am Leben seit: {}\n'
@@ -94,89 +95,97 @@ class TelegramBot(object):
                           .format(self.version, TelegramBot.format_date(self.started), self.messages))
 
     def cmd_config(self, bot, update):
-        if not self.is_admin(bot, update): return
+        if not self.is_admin(bot, update):
+            return
 
         self.send_message(update.message.chat_id, text='Aktion?',
                           reply_markup=self._reply_config)
 
         # Function handling the response
-        def _response(self, update):
-            if update.message.text == 'Inhalt':
-                with open(self.config_file) as fp:
+        def _response(resp_self, resp_update):
+            if resp_update.message.text == 'Inhalt':
+                with open(resp_self.config_file) as fp:
                     config_text = fp.read()
 
-                self.send_message(update.message.chat_id, text=config_text, reply_markup=telegram.ReplyKeyboardHide())
-                self.set_handle_response(update.message.chat_id, None)
+                resp_self.send_message(resp_update.message.chat_id,
+                                       text=config_text,
+                                       reply_markup=telegram.ReplyKeyboardHide())
+                resp_self.set_handle_response(resp_update.message.chat_id, None)
 
-            elif update.message.text == 'Neuladen':
+            elif resp_update.message.text == 'Neuladen':
                 self._reload_config_file()
-                self.send_message(update.message.chat_id, text='Erledigt', reply_markup=telegram.ReplyKeyboardHide())
-                self.set_handle_response(update.message.chat_id, None)
+                resp_self.send_message(resp_update.message.chat_id,
+                                       text='Erledigt',
+                                       reply_markup=telegram.ReplyKeyboardHide())
+                resp_self.set_handle_response(resp_update.message.chat_id, None)
 
-            elif update.message.text == 'Bearbeiten':
-                self.send_message(update.message.chat_id,
-                                  text='Inhalt der Config Datei eingeben!\nBenutze /cancel zum Abbrechen.',
-                                  reply_markup=telegram.ForceReply())
+            elif resp_update.message.text == 'Bearbeiten':
+                resp_self.send_message(resp_update.message.chat_id,
+                                       text='Inhalt der Config Datei eingeben!\nBenutze /cancel zum Abbrechen.',
+                                       reply_markup=telegram.ForceReply())
 
-                def _response_edit(self, update):
-                    resp = update.message.text
+                def _response_edit(edit_self, edit_update):
+                    resp = edit_update.message.text
                     new_config = None
 
                     try:
                         new_config = yaml.safe_load(resp)
                     except Exception as exp:
-                        self.send_message(update.message.chat_id,
-                                          text='Ungültige config: ' + str(
-                                              exp) + '!\nConfig erneut eingeben. Benutze /cancel zum Abbrechen.',
-                                          reply_markup=telegram.ForceReply())
+                        edit_self.send_message(edit_update.message.chat_id,
+                                               text='Ungültige config: ' +
+                                                    str(exp) +
+                                                    '!\nConfig erneut eingeben. Benutze /cancel zum Abbrechen.',
+                                               reply_markup=telegram.ForceReply())
                         return
 
-                    # Perform sone basic config checks
+                    # Perform some basic config checks
                     if 'users' not in new_config \
                             or 'admins' not in new_config \
                             or 'telegram_bot_token' not in new_config:
-                        self.send_message(update.message.chat_id,
-                                          text='Ungültige config\nConfig erneut eingeben. Benutze /cancel zum Abbrechen.',
-                                          reply_markup=telegram.ForceReply())
+                        edit_self.send_message(edit_update.message.chat_id,
+                                               text='Ungültige config\nConfig erneut eingeben. '
+                                                    'Benutze /cancel zum Abbrechen.',
+                                               reply_markup=telegram.ForceReply())
                         return
 
                     if new_config is not None:
-                        with open(self.config_file, 'w') as fp:
-                            fp.write(resp)
+                        with open(edit_self.config_file, 'w') as fp_config:
+                            fp_config.write(resp)
 
                         self._reload_config_file()
 
-                        self.send_message(update.message.chat_id, text='Erledigt',
-                                          reply_markup=telegram.ReplyKeyboardHide())
-                        self.set_handle_response(update.message.chat_id, None)
+                        edit_self.send_message(edit_update.message.chat_id, text='Erledigt',
+                                               reply_markup=telegram.ReplyKeyboardHide())
+                        edit_self.set_handle_response(edit_update.message.chat_id, None)
 
-                self.set_handle_response(update.message.chat_id, _response_edit)
+                resp_self.set_handle_response(resp_update.message.chat_id, _response_edit)
 
-
-            elif update.message.text == 'Abbrechen':
-                self.cmd_cancel(bot, update)
-
+            elif resp_update.message.text == 'Abbrechen':
+                resp_self.cmd_cancel(bot, resp_update)
             else:
-                self.send_message(update.message.chat_id, text='Bitte wähle eine der folgenden Möglichkeiten',
-                                  reply_markup=self._reply_config)
+                resp_self.send_message(resp_update.message.chat_id, text='Bitte wähle eine der folgenden Möglichkeiten',
+                                       reply_markup=self._reply_config)
 
         self.set_handle_response(update.message.chat_id, _response)
 
     def cmd_cancel(self, bot, update):
-        if not self.is_authorized(bot, update): return
+        if not self.is_authorized(bot, update):
+            return
 
         self.send_message(update.message.chat_id, text='Abgebrochen', reply_markup=telegram.ReplyKeyboardHide())
         self.set_handle_response(update.message.chat_id, None)
 
     def cmd_start(self, bot, update):
-        if not self.is_authorized(bot, update): return
+        if not self.is_authorized(bot, update):
+            return
         self.cmd_help(bot, update)
 
     def set_handle_response(self, chat_id, response_func):
         self._handle_response[chat_id] = response_func
 
     def rx_message(self, bot, update):
-        if not self.is_authorized(bot, update): return
+        if not self.is_authorized(bot, update):
+            return
 
         chat_id = update.message.chat_id
 
@@ -184,7 +193,8 @@ class TelegramBot(object):
         if chat_id in self._handle_response and self._handle_response[chat_id] is not None:
             self._handle_response[chat_id](self, update)
 
-    def bot_error(self, bot, update, error):
+    @staticmethod
+    def bot_error(bot, update, error):
         print('Update "%s" caused error "%s"' % (update, error))
 
     @staticmethod
@@ -204,7 +214,7 @@ class TelegramBot(object):
             # Determine directory this file or the calling function is located in
             frame = inspect.currentframe()
             if use_caller_version:
-                frame = inspect.getouterframes(frame)[2].frame # Got "up" to caller of constructor
+                frame = inspect.getouterframes(frame)[2].frame  # Got "up" to caller of constructor
             cwd = os.path.dirname(os.path.abspath(inspect.getfile(frame)))
 
             # try .version file
@@ -215,6 +225,7 @@ class TelegramBot(object):
                     return version
             else:
                 # try with git
-                return subprocess.check_output(['git', 'describe', '--long', '--always'], cwd=cwd).decode('utf8').strip()
+                return subprocess.check_output(['git', 'describe', '--long', '--always'], cwd=cwd).decode(
+                    'utf8').strip()
         except Exception:
             return '?'
